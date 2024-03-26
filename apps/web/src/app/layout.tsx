56c82b73b0b579f7d13c24bff5a8342102aaddaf
punpunkeshin05 <97283645+punpunkeshin05@users.noreply.github.com>
2024-03-26 19:47:03 +0700

    feat: font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Anuphan } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
+const anuphan = Anuphan({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={anuphan.className}>{children}</body>
     </html>
   );
 }
