f218d2ed6595ee8ee1460281679a4b756294b924
ImSoZRious <30285202+ImSoZRious@users.noreply.github.com>
2024-03-26 20:43:40 +0700

    simple count and display

--- apps/api/cmd/broadcaster.go
+++ apps/api/cmd/broadcaster.go
@@ -10,35 +10,34 @@ import (
 )
 
 type Broadcaster struct {
-  hub *Hub
-  cache *redis.Client
-  state *string
+	hub   *Hub
+	cache *redis.Client
+	state *string
 }
 
 func newBroadcaster(hub *Hub, cache *redis.Client) Broadcaster {
-  return Broadcaster{
-    hub: hub,
-    cache: cache,
-    state: nil,
-  }
+	return Broadcaster{
+		hub:   hub,
+		cache: cache,
+		state: nil,
+	}
 }
 
 func (b *Broadcaster) run() {
-  for {
-    time.Sleep(5 * time.Second)
-    ctx := context.Background()
-    state, err := b.cache.Get(ctx, "s:state").Result()
-    if err != nil {
-      if errors.Is(err, redis.Nil) {
-        continue
-      }
-      log.Printf("[ERROR] broadcaster: %v", err)
-      continue
-    }
-    if b.state == nil || *b.state != state {
-      b.state = &state
-      b.hub.broadcast <- []byte(state)
-    }
-  }
+	for {
+		time.Sleep(5 * time.Second)
+		ctx := context.Background()
+		state, err := b.cache.Get(ctx, "s:state").Result()
+		if err != nil {
+			if errors.Is(err, redis.Nil) {
+				continue
+			}
+			log.Printf("[ERROR] broadcaster: %v", err)
+			continue
+		}
+		if b.state == nil || *b.state != state {
+			b.state = &state
+			b.hub.broadcast <- []byte(state)
+		}
+	}
 }
-
