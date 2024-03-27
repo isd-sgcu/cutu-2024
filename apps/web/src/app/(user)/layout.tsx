284afbd6b19c136f04a5037c6544865d3599fbe8
teegoood <teegooodgood@gmail.com>
2024-03-27 21:07:36 +0700

    refactor: change shake sizing

--- apps/web/src/app/(user)/layout.tsx
+++ apps/web/src/app/(user)/layout.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import './style.css'
 
 export default function UserLayout({
   children,
--- apps/web/src/app/(user)/shake/components/Shake.tsx
+++ apps/web/src/app/(user)/shake/components/Shake.tsx
@@ -14,41 +14,32 @@ export default function ShakeComponent({
     const [isClicked, setIsClicked] = useState(false);
 
     return(
-        <div className="flex flex-col gap-5 px-[2.5rem] pt-[1.5rem] text-white items-center justify-center"
+        <div 
+            className="h-full px-[2.5rem] pt-[1.5rem] text-white"
             style={university == 'cu' ?
-                { background: 'linear-gradient(to bottom, #EE457B 0%, #F1678D 35%, #F6ADCD 90%, #FFFFFF 100%)' } :
-                { background: 'linear-gradient(to bottom, #AA1E23 0%, #F06143 35%, #FBAF44 90%, #FFFFFF 100%)' }
-            }
+                    { background: 'linear-gradient(to bottom, #EE457B 0%, #F1678D 35%, #F6ADCD 90%, #FFFFFF 100%)' } :
+                    { background: 'linear-gradient(to bottom, #AA1E23 0%, #F06143 35%, #FBAF44 90%, #FFFFFF 100%)' }
+                }
         >
-            <Image src="shake/cu-tu-ball-logo.svg" alt="cu-tu-ball-logo" width={65} height={104} ></Image>
-            <Image src={university == 'cu' ? "shake/chula-logo.svg" : "shake/tu-logo.svg"} alt="chula-logo" width={65} height={104} ></Image>
-            <h1 className="font-bold">{university == 'cu' ? '#ทีมจุฬาฯ' : '#ทีมมธ.'}</h1>
-            <p>ร่วมส่งพลังให้ทีมที่คุณเชียร์!</p>
-
-            <div onClick={() => setIsClicked(true)}>
-                {isClicked ? (
-                    <div className="text-center flex flex-col gap-[10px]">
-                        <div className={university === 'cu' ? "bg-[#F1678D] w-[245px] h-[245px] rounded-full flex items-center justify-center" : 
-                        "bg-[#EF5644] w-[245px] h-[245px] rounded-full flex items-center justify-center" }>
-                            <Image src="shake/shake-icon.svg" alt="shake-icon" width={153} height={158}></Image>
-                        </div>
-                        <h1 className="font-bold text-[24px] drop-shadow-md">เขย่าเลย!</h1>
-                    </div>
-                ) : (
-                    <div onClick={onClick} className="text-center flex flex-col gap-[10px]">
-                        <div className="bg-white w-[245px] h-[245px] rounded-full flex items-center justify-center">
-                            <Image src="shake/shake-icon-black.svg" alt="shake-icon" width={153} height={158}></Image>
-                        </div>
-                        <h1 className="font-bold text-[24px] drop-shadow-md">กดเพื่อเริ่มเขย่า!</h1>
-                    </div>
-                )}
-            </div>
-            <div className="self-end mb-[-15px]">
-                <Image src="shake/fire-icon.svg" alt="fire-icon" width={36} height={36}></Image>
-            </div>
-            <div className="w-full h-[16px] bg-white rounded-[20px] mb-[20px]">
-                <div className={university == 'cu' ? "h-full bg-[#EE477C] rounded-[20px]" : "h-full bg-[#FF6625] rounded-[20px]"} 
-                    style={{ width: `${(count / 700) * 100}%`}} />
+            <div className="h-full max-h-[600px] flex flex-col items-center justify-between">
+                <Image src="shake/cu-tu-ball-logo.svg" alt="cu-tu-ball-logo" width={60} height={104} ></Image>
+                <Image src={university == 'cu' ? "shake/chula-logo.svg" : "shake/tu-logo.svg"} alt="chula-logo" width={65} height={104} ></Image>
+                <h1 className="font-bold">{university == 'cu' ? '#ทีมจุฬาฯ' : '#ทีมมธ.'}</h1>
+                <p>ร่วมส่งพลังให้ทีมที่คุณเชียร์!</p>
+                <div onClick={() => setIsClicked(true)}>
+                    {isClicked ? (
+                        <Image src="shake/pink-shake.svg" alt="pink-shake-icon" width={150} height={150}></Image>
+                    ) : (
+                        <Image src="shake/white-shake.svg" alt="white-shake-icon" width={150} height={150}></Image>
+                    )}
+                </div>
+                <div className="self-end mb-[-15px]">
+                    <Image src="shake/fire-icon.svg" alt="fire-icon" width={36} height={36}></Image>
+                </div>
+                <div className="w-full h-[16px] bg-white rounded-[20px] mb-[20px]">
+                    <div className={university == 'cu' ? "h-full bg-[#EE477C] rounded-[20px]" : "h-full bg-[#FF6625] rounded-[20px]"}
+                        style={{ width: `${(count / 700) * 100}%`}} />
+                </div>
             </div>
         </div>
     )
