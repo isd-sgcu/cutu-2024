114d7be23c0e6117d030bf90de2ab40801030597
punchpnp <nunnapatppp@gmail.com>
2024-03-27 09:45:22 +0700

    feat: admin page

--- apps/web/src/app/admin/page.tsx
+++ apps/web/src/app/admin/page.tsx
@@ -1,11 +1,65 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div>
-      admin page
-    </div>
-  )
-}
+    <div
+      className="w-screen h-screen p-28"
+      style={{
+        background: "linear-gradient(to right, #F1678C, #ED1C24, #FBAF44)",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="h-1/2 flex flex-col justify-center items-center gap-20 pb-10">
+        <div className="flex-col space-y-6">
+          <Image src={"/admin/live.svg"} alt="live" width={200} height={200} />
+          <h1 className="text-3xl text-white">LIVE FEEDBACK</h1>
+        </div>
 
-export default page
+        <Image
+          src={"/admin/cu-tu-ball-logo.svg"}
+          alt="Cu Tu Ball Logo"
+          width={250}
+          height={250}
+        />
+        <h1 className="text-8xl text-white font-medium">For Admin</h1>
+      </div>
+      <div className="w-full h-1/2 flex justify-center flex-row space-x-60">
+        <div className="flex-col w-1/3">
+          <div className="bg-display rounded-[2rem] w-full h-3/4 flex justify-center items-center flex-col p-10 shadow-xl">
+            <Image
+              src={"/admin/display.svg"}
+              alt="feedback"
+              width={1000}
+              height={1000}
+            />
+          </div>
+          <div className="flex justify-center mt-10">
+            <button className="bg-white rounded-[2rem] w-2/3 px-20 py-10 text-8xl shadow-2xl hover:bg-slate-300 font-medium">
+              Display 1
+            </button>
+          </div>
+        </div>
+        <div className="flex-col w-1/3">
+          <div className="bg-display rounded-[2rem] w-full h-3/4 flex justify-end items-center flex-col p-10 shadow-xl">
+            <Image
+              src={"/admin/display.svg"}
+              alt="feedback"
+              width={1000}
+              height={1000}
+            />
+          </div>
+
+          <div className="flex justify-center mt-10">
+            <button className="bg-white rounded-[2rem] w-2/3 px-20 py-10 text-8xl shadow-2xl hover:bg-slate-300 font-medium">
+              Display 2
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
