cbf5eb1374d922ebe7d04735a7e71017f1e098bf
teegoood <teegooodgood@gmail.com>
2024-03-28 00:06:24 +0700

    add: back button

--- apps/web/src/components/Shake.tsx
+++ apps/web/src/components/Shake.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 import { useState } from "react"
 
 export default function ShakeComponent({
@@ -21,20 +22,23 @@ export default function ShakeComponent({
                     { background: 'linear-gradient(to bottom, #AA1E23 0%, #F06143 35%, #FBAF44 90%, #FFFFFF 100%)' }
                 }
         >
+            <Link href="../user" className="flex justify-start">
+                <Image src="../shake/arrow-back.svg" alt="arrow-back" width={25} height={25} />                    
+            </Link>
             <div className="h-full max-h-[600px] flex flex-col items-center justify-between">
-                <Image src="shake/cu-tu-ball-logo.svg" alt="cu-tu-ball-logo" width={60} height={104} ></Image>
-                <Image src={university == 'cu' ? "shake/chula-logo.svg" : "shake/tu-logo.svg"} alt="chula-logo" width={65} height={104} ></Image>
+                <Image src="../shake/cu-tu-ball-logo.svg" alt="cu-tu-ball-logo" width={60} height={104} ></Image>
+                <Image src={university == 'cu' ? "../shake/chula-logo.svg" : "../shake/tu-logo.svg"} alt="chula-logo" width={65} height={104} ></Image>
                 <h1 className="font-bold">{university == 'cu' ? '#ทีมจุฬาฯ' : '#ทีมมธ.'}</h1>
                 <p>ร่วมส่งพลังให้ทีมที่คุณเชียร์!</p>
                 <div onClick={() => setIsClicked(true)}>
                     {isClicked ? (
-                        <Image src="shake/pink-shake.svg" alt="pink-shake-icon" width={150} height={150}></Image>
+                        <Image src="../shake/pink-shake.svg" alt="pink-shake-icon" width={150} height={150}></Image>
                     ) : (
-                        <Image src="shake/white-shake.svg" alt="white-shake-icon" width={150} height={150}></Image>
+                        <Image src="../shake/white-shake.svg" alt="white-shake-icon" width={150} height={150}></Image>
                     )}
                 </div>
                 <div className="self-end mb-[-15px]">
-                    <Image src="shake/fire-icon.svg" alt="fire-icon" width={36} height={36}></Image>
+                    <Image src="../shake/fire-icon.svg" alt="fire-icon" width={36} height={36}></Image>
                 </div>
                 <div className="w-full h-[16px] bg-white rounded-[20px] mb-[20px]">
                     <div className={university == 'cu' ? "h-full bg-[#EE477C] rounded-[20px]" : "h-full bg-[#FF6625] rounded-[20px]"}
