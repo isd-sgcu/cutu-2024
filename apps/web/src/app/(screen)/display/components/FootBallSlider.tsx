4cc6ded27739805512c7ff2f4321039f577a9d4f
Sirawit Chanaburanasak <punpunsirawit@gmail.com>
2024-03-29 05:08:54 +0700

    Co-authored-by: Thanapat Chotipun <PatrickChoDev@users.noreply.github.com> (#9)
    Co-authored-by: tntons <tntons@users.noreply.github.com>
    Co-authored-by: ImSoZRious <ImSoZRious@users.noreply.github.com>
    
    Co-authored-by: tntons <108630823+tntons@users.noreply.github.com>

--- apps/web/src/app/(screen)/display/components/FootBallSlider.tsx
+++ apps/web/src/app/(screen)/display/components/FootBallSlider.tsx
@@ -3,6 +3,10 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import { useState } from "react";
+import { io } from "socket.io-client";
+import FingerprintJS from "@fingerprintjs/fingerprintjs";
+import Cookies from "js-cookie";
+import {v4 as uuidv4} from 'uuid';
 
 interface FootBallSliderProps {
   sliderHeight: string;
@@ -17,7 +21,59 @@ const FootBallSlider = (props: FootBallSliderProps) => {
   const [position, setPosition] = useState(0);
   const [tu, setTu] = useState(1);
   const [cu, setCu] = useState(1);
+  const [cid, setCid] = useState<string | null>(null);
+  let fid: string | null = null;
 
+  const socket = io("wss://api.cutu2024.sgcu.in.th", {
+    path: "/api/ws",
+    auth: { fid: "1234", name: "hehe", cid: '86ccc208-46ef-488c-acbe-f827629b502d' },
+    transports: ['websocket'],
+  });
+
+  function onConnect() {
+    console.log("connect")
+    console.log(socket.id, socket.connected)
+  }
+  function onDisconnect() {
+    console.log("disconnect")
+    console.log(socket.id, socket.connected)
+  }
+
+  useEffect(() => {
+    socket.on("cid", (serverCid) => {
+      console.log(serverCid);
+      setCid(serverCid);
+      Cookies.set("cid", serverCid, { expires: 365 * 100000 });
+    });
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", (reason, details) => {
+      console.log(reason);
+
+      // the low-level reason of the disconnection, for example "xhr post error"
+      console.log(details);
+    });
+
+    socket.on("events", (serverCid) => {
+      console.log(serverCid);
+    });
+
+    socket.on("state", (serverCid) => {
+      console.log(serverCid);
+    });
+
+    socket.on("scoreboard", (serverCid) => {
+      console.log(serverCid);
+      const part = serverCid.split(" ");
+
+      const cuScore = parseInt(part[1]);
+      const tuScore = parseInt(part[2]);
+
+      setCu(cuScore);
+      setTu(tuScore);
+      console.log(position, cu, tu);
+    });
+  }, [])
   useEffect(() => {
     //console.log((tu - cu)/(tu + cu) * MAX_LENGTH)
     setPosition((tu - cu)/(tu + cu) * MAX_LENGTH)
